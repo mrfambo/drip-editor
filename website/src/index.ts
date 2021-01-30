3ddd58fb3b8194f0597ffbfcbdfa60520cd4e253
Ali Muqaddas <alimuqaddas92@gmail.com>
2021-01-30 09:49:10 +0500

    Added eslint

--- website/src/index.ts
+++ website/src/index.ts
@@ -1,1 +1,9 @@
+/*
+    Entry point for Component library
+
+    - Make sure when you are adding any package here, you add the same on root
+    - Rollup runs on root folder
+    - NextJS is running here to give you development experiance and deployment for demo
+*/
 export { Editor } from './components/Editor'
+
