ffa23a7545dd15de83920c7708616b1e495823e6
Ali Muqaddas <alimuqaddas92@gmail.com>
2021-01-31 02:43:48 +0500

    refactor: :recycle: refactoring and added test suite

--- src/components/Editor/Editor.test.js
+++ src/components/Editor/Editor.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
-import { Editor } from './Editor'
+import Editor from './Editor'
 
 describe('<Editor />', () => {
   it('should render', () => {
